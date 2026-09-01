418f80ea125a18483437f135ef76c67f23d82d4e
Hardik Sorthiya <sorthiyahardik6869@gmail.com>
2026-09-01 13:23:13 +0530

    feat(middleware): expand error handler for Mongoose specific errors

--- middleware/error.js
+++ middleware/error.js
@@ -1,8 +1,31 @@
+const ErrorResponse = require("../utils/errorresponse");
+
 const errorHandler = (err, req, res, next) => {
-  res.status(err.statusCode || 500).json({
+  let error = { ...err };
+  error.message = err.message;
+
+  if (err.name === "CastError") {
+    const message = `Resource not found with id of ${err.value}`;
+    error = new ErrorResponse(message, 404);
+  }
+
+  // Mongoose duplicate key
+  if (err.code === 11000) {
+    const message = "Duplicate field value entered";
+    error = new ErrorResponse(message, 400);
+  }
+
+  // Mongoose validation error
+  if (err.name === "ValidationError") {
+    const message = Object.values(err.errors).map((val) => val.message);
+    error = new ErrorResponse(message, 400);
+  }
+
+  res.status(error.statusCode || 500).json({
     success: false,
-    error: err.message || "Server Error",
+    error: error.message || "Server Error",
   });
+  x;
 };
 
 module.exports = errorHandler;
