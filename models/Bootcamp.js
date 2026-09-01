e06a553b9a2397aafe455f7d4dd6b0215c1e4688
Hardik Sorthiya <sorthiyahardik6869@gmail.com>
2026-09-01 14:17:05 +0530

    feat(models): add slugify pre-save middleware to Bootcamp schema

--- models/Bootcamp.js
+++ models/Bootcamp.js
@@ -1,4 +1,5 @@
 const mongoose = require("mongoose");
+const slugify = require("slugify");
 
 const BootcampSchema = new mongoose.Schema({
   name: {
@@ -100,6 +101,11 @@ const BootcampSchema = new mongoose.Schema({
     type: Date,
     default: Date.now,
   },
+});
+
+// Create bootcamp slug from the name
+BootcampSchema.pre("save", function () {
+  this.slug = slugify(this.name, { lower: true });
 });
 
 module.exports = mongoose.model("Bootcamp", BootcampSchema);
