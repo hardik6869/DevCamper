4c0c1a7c6d90f64d68290d1d1484e8663da25a29
Hardik Sorthiya <sorthiyahardik6869@gmail.com>
2026-09-01 08:48:44 +0530

    feat(db): connect to database with Mongoose

--- server.js
+++ server.js
@@ -1,19 +1,31 @@
 const express = require("express");
 const dotenv = require("dotenv");
-
-// Route Files
-const bootcamps = require("./routes/bootcamps");
+const morgan = require("morgan");
+const connectDB = require("./config/db");
 
 // Load ENV File
 dotenv.config({ path: "./config/config.env" });
 
+connectDB();
+
+// Route Files
+const bootcamps = require("./routes/bootcamps");
+
 // Initialize Express
 const app = express();
 
+if (process.env.NODE_ENV === "development") {
+  app.use(morgan("dev"));
+}
+
 // Mount Routers
-app.use('/api/v1/bootcamps', bootcamps);
+app.use("/api/v1/bootcamps", bootcamps);
 
 const PORT = process.env.PORT || 5000;
 
+const server = app.listen(PORT, console.log(`Server running on ${PORT}`));
 
-app.listen(PORT, console.log(`Server running on ${PORT}`));
+process.on("unhandledRejection", (err, promise) => {
+  console.log(`Error: ${err.message}`);
+  server.close(() => process.exit(1));
+});
