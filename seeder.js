51eee5aa78ba62e11355739ef5fe5897a8b91e4f
Hardik Sorthiya <sorthiyahardik6869@gmail.com>
2026-09-02 10:23:53 +0530

    feat(courses): add Course model and seed script

--- seeder.js
+++ seeder.js
@@ -7,19 +7,24 @@ dotenv.config({ path: "./config/config.env" });
 
 // load models
 const Bootcamp = require("./models/Bootcamp");
+const Course = require("./models/Course");
 
 // connect to DB
 mongoose.connect(process.env.MONGO_URI);
-    
+
 /// Read JSON files
-const Bootcamps = JSON.parse(
+const bootcamps = JSON.parse(
   fs.readFileSync(`${__dirname}/_data/bootcamps.json`, "utf-8"),
+);
+const courses = JSON.parse(
+  fs.readFileSync(`${__dirname}/_data/courses.json`, "utf-8"),
 );
 
 // import into DB
 const importData = async () => {
   try {
-    await Bootcamp.create(Bootcamps);
+    await Bootcamp.create(bootcamps);
+    await Course.create(courses);
     console.log("Data Imported...".green.inverse);
     process.exit();
   } catch (err) {
@@ -31,6 +36,7 @@ const importData = async () => {
 const deleteData = async () => {
   try {
     await Bootcamp.deleteMany();
+    await Course.deleteMany();
     console.log("Data Destroyed...".red.inverse);
     process.exit();
   } catch (err) {
