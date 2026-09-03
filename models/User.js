c1307aec5f097e45408ccee6ab2b6efa15e1c6ed
Hardik Sorthiya <sorthiyahardik6869@gmail.com>
2026-09-03 08:27:18 +0530

    feat(auth): create User Mongoose model and schema

---
+++ models/User.js
@@ -0,0 +1,40 @@
+const mongoose = require("mongoose");
+
+const UserSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, "Please add a name"],
+    unique: true,
+    trim: true,
+    maxlength: [50, "Name can not be more than 50 characters"],
+    minlength: [3, "Name can not be less than 3 characters"],
+  },
+  email: {
+    type: String,
+    required: [true, "please addd an email"],
+    unique: true,
+    match: [
+      /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/,
+      "Please add a valid email",
+    ],
+  },
+  role: {
+    type: String,
+    enum: ["user", "publisher"],
+    default: "user",
+  },
+  password: {
+    type: String,
+    required: [true, "Please Add a password"],
+    minlength: 6,
+    select: false,
+  },
+  resetPasswordToken: String,
+  resetPasswordExpire: Date,
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+module.exports = mongoose.model("User", UserSchema);
