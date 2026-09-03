f7fb784b1c75086c629129ab99832a039a373a54
Hardik Sorthiya <sorthiyahardik6869@gmail.com>
2026-09-03 09:32:37 +0530

    feat(auth): sign and return JSON Web Token on user registration

--- controllers/auth.js
+++ controllers/auth.js
@@ -14,7 +14,12 @@ exports.register = asyncHandler(async (req, res, next) => {
     email,
     password,
     role,
-  }); 
+  });
 
-  res.status(200).json({ success: true });
+  
+
+  // Create Token
+  const token = user.getSignedJwtToken();
+
+  res.status(200).json({ success: true, token: token });
 });
